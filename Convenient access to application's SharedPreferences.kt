b21ea0997c3b1a07b2d5e1428c9b5f4ffa9520c9
Michael Zuskin <90226705+Ursego@users.noreply.github.com>
2025-12-29 06:41:23 -0500

    Improve comments and update preference library version
    Refactor comments for clarity and update dependency version.

--- Convenient access to application's SharedPreferences.kt
+++ Convenient access to application's SharedPreferences.kt
@@ -1,4 +1,7 @@
-// Non-String values could be stored in SharedPreferences by PreferenceFragment converted to String. If you read such a preference, you have to remember about that, and cast back to the original type if necessary. The suggested helper object AppPrefs hides all that complexity: if you want an Int or a Boolean, you simply call getInt() or getBoolean() - that's it. You will get the value even if it was stored as String.
+// Non-String values could be stored in SharedPreferences converted to String.
+// If you read such a preference, you have to remember about that, and cast back to the original type if necessary.
+// The suggested helper object AppPrefs hides all that complexity: if you want an Int or a Boolean, you simply call getInt() or getBoolean().
+// You will get the value even if it was stored as String.
 
 // That object also provides the put() function (overloaded for different types) which simplifies writing into SharedPreferences, hiding in itself all the magic with the editor.
 
@@ -6,19 +9,21 @@
 
 AppPrefs.put(PrefKey.APP_LANGUAGE, selectedLanguage, context)
 ...
-val currentLanguage = AppPrefs.getString(PrefKey.APP_LANGUAGE, context)
+val currLanguage = AppPrefs.getString(PrefKey.APP_LANGUAGE, context)
 
 // STEPS:
 
-// @ Open your app-level build.gradle file and check if its dependencies section contains the following line (the version 1.1.0 was the latest when this topic was created - in the time you read it the number could be higher):
+// @ Open your app-level build.gradle file and check if its dependencies section contains the following line (the version 1.1.0 was the latest when this topic was created -
+// in the time you read it the number could be higher):
 
-implementation 'androidx.preference:preference:1.1.0'
+implementation 'androidx.preference:preference:1.2.1'
 
-// If the line is not there, add it. If Android Studio has changed the background color in the added line, change the version to the latest one (click the line, press Alt+Enter and select the option "Change to ...").
+// If the line is not there, add it.
+// If Android Studio has changed the background color in the added line, change the version to the latest one (click the line, press Alt+Enter and select the option "Change to ...").
 
-// @ Create object Chronos as described here.
+// @ Create object Chronos as described here: https://tinyurl.com/ChronosObj
 
-// @ In "util" package, created in the previous step, create object AppPrefs and copy to it the source code, provided below (just after the package statement):
+// @ In the "util" package, created in the previous step, create object AppPrefs and copy to it the source code, provided below (just after the package statement):
 
 import android.content.Context
 import androidx.preference.PreferenceManager
