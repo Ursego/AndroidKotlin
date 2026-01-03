e1fb163eac6108131947f8c639fe1db1901c4ccd
Michael Zuskin <90226705+Ursego@users.noreply.github.com>
2026-01-03 05:17:40 -0500

    Update Convert date:time related types to String and back.kt

--- Convert date:time related types to String and back.kt
+++ Convert date:time related types to String and back.kt
@@ -12,58 +12,62 @@ Encapsulates the fuss with date/time formats and parsing.
 Useful when a time/date value, which should be manipulated in code in its original type,
 is stored somewhere as String (for example, in Preferences or SQLite DB).
 Chronos is the personification of time in pre-Socratic philosophy and later literature (https://en.wikipedia.org/wiki/Chronos)
+https://tinyurl.com/ChronosObj
 ****************************************************************************************************************************/
 
 object Chronos {
-    val dateFormatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE // DateTimeFormatter.ofPattern("<your format>")
-    val timeFormatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_TIME // DateTimeFormatter.ofPattern("<your format>")
-    val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME // DateTimeFormatter.ofPattern("<your format>")
+    val dFormatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE // or: DateTimeFormatter.ofPattern("<your pattern>")
+    val tFormatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_TIME // or: DateTimeFormatter.ofPattern("<your pattern>")
+    val dtFormatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME // or: DateTimeFormatter.ofPattern("<your pattern>")
 
     // LocalDate:
 
-    fun toLocalDate (valAsString: String?): LocalDate? {
-        if (valAsString == null) return null
+    fun toLocalDate(sVal: String?): LocalDate? {
+        if (sVal == null) return null
         try {
-            return LocalDate.parse(valAsString, dateFormatter)
+            return LocalDate.parse(sVal, dFormatter)
         } catch (e: DateTimeParseException) {
-            throw Exception("Cannot parse '$valAsString' to LocalDate.")
+            throw Exception("Cannot parse '$sVal' to LocalDate.")
         }
     }
 
-    fun toString (valAsLocalDate: LocalDate?): String? {
-        if (valAsLocalDate == null) return null
-        return dateFormatter.format(valAsLocalDate)
+    fun toString(dVal: LocalDate?, pattern: String? = null): String? {
+        if (dVal == null) return null
+        val f = if (pattern.isNullOrBlank()) dFormatter else DateTimeFormatter.ofPattern(pattern)
+        return f.format(dVal)
     }
 
     // LocalTime:
 
-    fun toLocalTime (valAsString: String?): LocalTime? {
-        if (valAsString == null) return null
+    fun toLocalTime(sVal: String?): LocalTime? {
+        if (sVal == null) return null
         try {
-            return LocalTime.parse(valAsString, timeFormatter)
+            return LocalTime.parse(sVal, tFormatter)
         } catch (e: DateTimeParseException) {
-            throw Exception("Cannot parse '$valAsString' to LocalTime.")
+            throw Exception("Cannot parse '$sVal' to LocalTime.")
         }
     }
 
-    fun toString (valAsLocalTime: LocalTime?): String? {
-        if (valAsLocalTime == null) return null
-        return timeFormatter.format(valAsLocalTime)
+    fun toString(tVal: LocalTime?, pattern: String? = null): String? {
+        if (tVal == null) return null
+        val f = if (pattern.isNullOrBlank()) tFormatter else DateTimeFormatter.ofPattern(pattern)
+        return f.format(tVal)
     }
 
     // LocalDateTime:
 
-    fun toLocalDateTime (valAsString: String?): LocalDateTime? {
-        if (valAsString == null) return null
+    fun toLocalDateTime(sVal: String?): LocalDateTime? {
+        if (sVal == null) return null
         try {
-            return LocalDateTime.parse(valAsString, dateTimeFormatter)
+            return LocalDateTime.parse(sVal, dtFormatter)
         } catch (e: DateTimeParseException) {
-            throw Exception("Cannot parse '$valAsString' to LocalDateTime.")
+            throw Exception("Cannot parse '$sVal' to LocalDateTime.")
         }
     }
 
-    fun toString (valAsLocalDateTime: LocalDateTime?): String? {
-        if (valAsLocalDateTime == null) return null
-        return dateTimeFormatter.format(valAsLocalDateTime)
+    fun toString(dtVal: LocalDateTime?, pattern: String? = null): String? {
+        if (dtVal == null) return null
+        val f = if (pattern.isNullOrBlank()) dtFormatter else DateTimeFormatter.ofPattern(pattern)
+        return f.format(dtVal)
     }
 }
